02ac94565251913d6364727ba205ef90679be696
sharmakajal0 <sharma.kajal.sitm@gmail.com>
2019-07-22 16:03:46 +0530

    Function Definition to find probability in a list of events

--- mathematics/probability/p1.py
+++ mathematics/probability/p1.py
@@ -5,19 +5,21 @@ from sys import argv
 
 def probability(event, events):
 
-  '''Function Definition for finding probability in a list of events'''
-  event_count = 0
+    '''Function Definition for finding probability in a list of events'''
+    event_count = 0
 
-  for i in events:
-    if i == event:
-      event_count += 1
+    for i in events:
+        if i == event:
+            event_count += 1
 
-  return event_count / len(events)
+    return event_count / len(events)
 
 def main(args):
-  events = [1, 2, 3, 4, 5, 4, 2, 2, 4, 56, 6, 3, 2, 12, 3, 4, 5]
-  event = int(args[1])
-  print(probability(event, events))
+
+    '''function to take arguments'''
+    events = [1, 2, 3, 4, 5, 4, 2, 2, 4, 56, 6, 3, 2, 12, 3, 4, 5]
+    event = int(args[1])
+    print(probability(event, events))
 
 if __name__ == "__main__":
-  main(argv)
+    main(argv)
